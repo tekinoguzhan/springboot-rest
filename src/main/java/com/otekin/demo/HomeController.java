a87ea1ca13b41ad6e8d220d53ead06bbf0939c6a
Oğuzhan Tekin <119113636+tekinoguzhan@users.noreply.github.com>
2023-09-24 21:41:54 +0300

    Add files via upload

---
+++ src/main/java/com/otekin/demo/HomeController.java
@@ -0,0 +1,24 @@
+package com.otekin.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class HomeController {
+
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public ResponseEntity<List<UserVo>> listAllUsers() {
+		List<UserVo> list = new ArrayList<>();
+		UserVo obj = new UserVo();
+		obj.setName("knowledgefactory");
+		obj.setEmail("knowledgefactory4upeoples@gmail.com");
+		list.add(obj);
+		return new ResponseEntity<List<UserVo>>(list, HttpStatus.OK);
+	}
+}
